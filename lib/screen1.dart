52b8bf02eec6d3b6d37923fbd49b389af76e7928
ggcsriram <ggcsriram@gmail.com>
2019-06-19 18:14:03 +0530

    Creating screen1 file

---
+++ lib/screen1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatefulWidget {
+  @override
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
